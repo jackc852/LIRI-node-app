2fcf1c97c4935429d1242ee377559698a9c2af3f
Jack Chiu <jchk852@yahoo.com>
2018-04-28 13:18:33 -0700

    twitter up

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+    consumer_key: '26TQNm4jMiFDOW1oCwDYi5QM4',
+    consumer_secret: 'Soj6YtLHX8J4NZcELDZ68GzBW6Ub51m5H8GjHQswQBm7A0AlPS',
+    access_token_key: '2516580300-E3yWemdwfMODNTcVfKcp1ZSB7lxG1oOUkU1Idu8',
+    access_token_secret: 'EYewJLAkBsIOY75wRsLA6eOqjSlKHJxt0jKEkfXDNmaq5',
+}
--- liri.js
+++ liri.js
@@ -4,11 +4,104 @@ var twitter = require('twitter');
 var spotify = require('spotify');
 var fs = require('fs');
 
-var client = new twitter(keys.twitterKeys)
+var getTweets = function() {
+    var client = new twitter(keys.twitterKeys)
+    
+    var params = {screen_name: 'hkdmjack'};
+    client.get('statuses/user_timeline', params, function(error, tweets, response ){
+        if (!error) {
+            // console.log(tweets);
+            for(var i=0; i<tweets.length; i++) {
+                console.log(tweets[i].created_at);
+                console.log(' ');
+                console.log(tweets[i].test);
+            }
+        }
+    });
+    
+}
 
-var params = {screen_name: 'hkdmjack'};
-client.get('statuses/user_timeline', params, function(error, tweets, response ){
-    if (!error) {
-        console.log(tweets);
+var getArtist = function(artist) {
+    return artist.name;
+}
+
+var getSpotify = function(songName) {
+    
+    spotify.search({ type: 'track', query: 'dancing in the moonlight' }, function(err, data) {
+        if ( err ) {
+            console.log('Error occurred: ' + err);
+            return;
+        }
+        
+        var songs = data.tracks.items;
+        for(var i=0; i<songs.length; i++) {
+            console.log(i);
+            console.log('artist(s): ' + songs[i].artists.map(getArtist));
+            console.log('song name: ' + songs[i].name);
+            console.log('preview song: ' + songs[i].preview_url);
+            console.log('album: ' + songs[i].album.name);
+        }
+    });
+    
+}
+
+var getMovie = function(movieName) {
+    
+    request('http://www.omdbapi.com/?t=' + movieName + '&y=&plot=short&r=json', function (error, response, body) {
+    if(!error && response.statusCode == 200) {
+        
+        var jsonData = JSON.parse(body);
+        
+        console.log('Title: ' +jsonData.Title);
+        console.log('Year: ' +jsonData.Year);
+        console.log('Rated: ' + jsonData.Rated);
+        console.log('IMDB Rating: ' +jsonData.imdbRating);
+        console.log('Country: ' + jsonData.Country);
+        console.log('Language: ' + jsonData.Language);
+        console.log('Plot: ' + jsonData.Plot);
+        console.log('Actors: ' + jsonData.Actors);
+        console.log('Rotten Tomatoes rating: ' + jsonData.tomatoRating);
+        console.log('Rotten Tomatoes URL ' + jsonData.tomatoURL);
     }
 });
+}
+
+var doWhatItSays = function() {
+    
+    fs.readFile('random.txt', 'utf8', function (err, data) {
+        if (err) throw err;
+        
+        var dataArr = data.split(',');
+        
+        if (dataArr.length == 2) {
+            pick(dataArr[0], dataArr[1]);
+        } else if (dataArr.length == 1) {
+            pick(dataArr[0]);
+        }
+    });
+}
+
+var choice = function(caseData, functionData) {
+    switch(caseData) {
+        case 'my-tweets' :
+        getTweets();
+        break;
+        case 'spotify-this-song':
+        getSpotify(functionData);
+        break;
+        case 'movie-this':
+        getMovie(functionData);
+        case 'do-what-it-says':
+        doWhatItSays();
+        break;
+        default:
+        console.log('LIRI cannot do that');
+    }
+}
+
+var runTHis = function(argOne, argTwo) {
+    choice(argOne, argTwo);
+};
+
+runTHis(process.argv[2], process.argv[3]);
+
